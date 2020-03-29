f9fbab96bc170dde1be51f7cee31ca89b61fb3ea
Abhinav Tushar <abhinav@lepisma.xyz>
2020-03-29 23:01:19 +0530

    add resource type

---
+++ jaglar/tj.py
@@ -0,0 +1,15 @@
+"""
+Module for interacting with tj. Mostly dumping tasks in tj3 file.
+"""
+
+from typing import List
+
+from jaglar.types import Resource, Task
+
+
+def write_to_tj_file(tasks: List[Task], resources: List[Resource], file_path: str):
+    """
+    Write given tasks to given file in tj format.
+    """
+
+    raise NotImplementedError()
--- jaglar/types.py
+++ jaglar/types.py
@@ -2,8 +2,14 @@ from typing import List
 
 
 @dataclass
+class Resource:
+    name: str
+    hours_per_day: int = 8
+
+
+@dataclass
 class Task:
     name: str
-    assignee: str
-    time: float
+    assignee: List[Resource]
+    effort: float
     depends_on: List["Task"]
