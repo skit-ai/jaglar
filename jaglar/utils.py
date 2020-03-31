857727b79cf7004424ed22a20637814374b0338e
Abhinav Tushar <abhinav@lepisma.xyz>
2020-03-31 11:20:28 +0530

    fix map_keys to work with list nesting

--- jaglar/utils.py
+++ jaglar/utils.py
@@ -7,9 +7,11 @@ def map_keys(d: Dict, fn) -> Dict:
     """
 
     new_d = {}
-    for k, v in d:
+    for k, v in d.items():
         if isinstance(v, dict):
             new_d[fn(k)] = map_keys(v, fn)
+        elif isinstance(v, list):
+            new_d[fn(k)] = [map_keys(it, fn) for it in v]
         else:
             new_d[fn(k)] = v
 
